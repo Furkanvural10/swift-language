d994c8160423b462d83ac0f219af69eeea411b73
Furkan Vural <furkanvural52@gmail.com>
2023-02-10 16:38:53 +0300

    completed data types topic

--- DataTypes.playground/Contents.swift
+++ DataTypes.playground/Contents.swift
@@ -1,3 +1,56 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// This sign (//) represents comment-line. While code run, comment-line does no run.
+
+/*
+ If you want to use multiple comment line you must use (/* your comment */)
+ Comments-line helps us to understading and reading easily code
+ */
+
+
+// Variable and Constant
+
+// Variable - using "var" keyword
+var userName = "Joe"
+var userSurname = "Burry"
+
+// Constant - using "let" keyword.
+
+let piNumber = 3.14
+let userID = "osdkf92if92jkfds01md9"
+
+// ** Important note: The difference between Variable and Constant is that variable's value can be changed later, but constan can not. Example below
+
+userName = "Eric" // Previously userName equalts to "Joe"
+userSurname = "Keept" // Previously userSurname equalts to "Burry"
+
+/*
+ 
+ Data Types
+ 1 - String
+ 2 - Integer (Briefly, we show "int")
+ 3 - Double
+ 4 - Boolean (Briefly, we show "bool")
+ 
+*/
+
+// String --> Any data enclosed in double quotes is considered a String.
+let bookName = "The Lord of the Rings"
+let numberOfBookPage = "456" // Yes, it is a string, because data is enclosed in double quotes.
+
+// Integer --> Expresses negative or positive integer.
+var userAge = 25
+var userWeight = 79
+
+// Double -->  Negative or positive decimal numbers double.
+let pi = 3.14
+let negativeDoubleNumber = -123.5
+
+// Boolean --> True or false. When naming Boolean variables, we usually define it as isXX.
+var isUserActive = true
+var isColorPurple = false
+ 
+ 
+
+
+
