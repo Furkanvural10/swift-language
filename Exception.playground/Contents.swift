eeda9bdddd8b1718570f2f48c4dc1d5f377c3869
Furkan Vural <furkanvural52@gmail.com>
2023-03-06 23:58:59 +0300

    Added exception definition

---
+++ Exception.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+/* ****** EXCEPTION ********
+ 
+ There are two types of exception.
+ 1- Run time ---> Folder is not found, simulator crash
+ 2- Compiler Error ---> Syntax error
+ 
+ */
+
+// DO TRY CATCH
+    
+
+
+ 
