43df4fd1432bc0a9b1800da4b292aedca61b25a7
Furkan Vural <furkanvural52@gmail.com>
2023-03-08 00:05:50 +0300

    Added exception example

--- Exception.playground/Contents.swift
+++ Exception.playground/Contents.swift
@@ -9,7 +9,27 @@ import UIKit
  */
 
 // DO TRY CATCH
-    
+
+enum Errors: Error {
+    case zeroNumberError
+}
+
+func divideTwoNumber(firstNumber: Int, secondNumber: Int) throws -> Int {
+    if firstNumber == 0 || secondNumber == 0{
+        throw Errors.zeroNumberError
+    }
+    return firstNumber + secondNumber
+}
+
+do {
+    let result = try divideTwoNumber(firstNumber: 1, secondNumber: 0)
+    print(result)
+} catch Errors.zeroNumberError {
+    print("Number is not be zero")
+}
+
+
+
 
 
  
