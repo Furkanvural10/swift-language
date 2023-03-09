8318a2e6c31782334657f499a13c1324ad31cb46
Furkan Vural <furkanvural52@gmail.com>
2023-03-09 18:31:18 +0300

    Added mutating function feature

--- Struct.playground/Contents.swift
+++ Struct.playground/Contents.swift
@@ -46,3 +46,62 @@ struct Car2 {
 }
 
 let bmw = Car2(carName: "bmw m4", carPriece: 70_000, carColor: .purple)
+
+
+
+// Mutating Functions
+
+class User {
+    
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    func celebrateBirthday(){
+        age += 1
+        // You can show the celebration message
+    }
+    
+}
+
+var henderson = User(name: "Henderson", age: 30)
+print(henderson.age)
+// Output: 30
+
+henderson.celebrateBirthday()
+print(henderson.age)
+// Output: 31.
+
+
+// We use the mutating function in struct.
+
+struct Person {
+    var name: String
+    var age: Int
+    
+    func celebrateBirthday(){
+        
+        /* age += 1
+         Give us error: Left side of mutating operator isn't mutable: 'self' is immutable.
+         If you want to increase age variable we must use "mutating" key word.
+         */
+    }
+    
+    mutating func celebBirthday(){
+        age += 1
+    }
+}
+
+var thomas = Person(name: "Thomas", age: 29)
+print(thomas.age)
+// Output: 29.
+
+thomas.celebBirthday()
+print(thomas.age)
+// Output: 30.
+
+
