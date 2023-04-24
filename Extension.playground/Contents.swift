1336d867bfb46a0121befdc0944fbaa634f68ec2
Furkan Vural <furkanvural52@gmail.com>
2023-04-25 01:59:49 +0300

    Added extension topic

--- Extension.playground/Contents.swift
+++ Extension.playground/Contents.swift
@@ -2,7 +2,40 @@ import UIKit
 
 // MARK: - Extension
 
-// Create your own custom print
+// The main purpose of extensions is to change classes that we cannot access.
+
+// Create your own custom print function
+
+extension String {
+    func makeCustomPrint(){
+        print(self)
+    }
+}
+
+"Print this custom string".makeCustomPrint()
+
+// MARK: - Rules
+// 1- You don't add stored properties to Extensions like --> var age: Int
+// 2- You can add any functions that you want
+
+// MARK: - Extend your own Class or Struct
+
+struct User {
+    var name: String
+    var age: Int
+}
+
+extension User {
+    func printUserInfo(){
+        print("Name: \(name), age: \(age)")
+    }
+}
+
+// Create Instance from User struct
+var john = User(name: "John", age: 24)
+john.printUserInfo()
+// output: Name: John, age: 24
+
 
 
 
