9f8d2548b541ca38c28335048d60e40ecc82cf0e
Furkan Vural <furkanvural52@gmail.com>
2023-10-03 20:38:05 +0300

    Added first example

--- PropertyWrappers.playground/Contents.swift
+++ PropertyWrappers.playground/Contents.swift
@@ -1,5 +1,37 @@
 import UIKit
+import Foundation
 
 // MARK: - Property Wrappers
+
+@propertyWrapper
+struct Uppercased {
+    
+    private var value: String
+    
+    var wrappedValue: String {
+        get {
+            value.localizedUppercase
+        }
+        set {
+            value.localizedUppercase
+        }
+    }
+    
+    init(wrappedValue: String) {
+        self.value = wrappedValue
+    }
+    
+    
+}
+
+struct User {
+    
+    @Uppercased
+    var name: String
+}
+
+var firstUser = User(name: "john")
+print(firstUser.name)
+
 
 
