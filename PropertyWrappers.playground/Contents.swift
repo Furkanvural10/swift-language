23118e1375fcbc6743e2753a686011fec8a50d07
Furkan Vural <furkanvural52@gmail.com>
2023-10-03 21:30:55 +0300

    Added second example

--- PropertyWrappers.playground/Contents.swift
+++ PropertyWrappers.playground/Contents.swift
@@ -34,4 +34,25 @@ var firstUser = User(name: "john")
 print(firstUser.name)
 
 
+@propertyWrapper
+struct DateFormat {
+    
+    private var value: Double = 0
+    
+    var wrappedValue: String {
+        get {
+            String(format: "%.2f", value)
+        }
+        set {
+            value = Double(newValue) ?? 0
+        }
+    }
+}
 
+struct Product {
+    @DateFormat var price: String
+}
+
+var prod1 = Product()
+prod1.price = "123.345"
+print(prod1.price)
