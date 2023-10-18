c29f51b15c05e14e129c016f2d5acbc572d0eb1d
Furkan Vural <furkanvural52@gmail.com>
2023-10-18 22:28:31 +0300

    Add custom decoding (codingKeys)

--- Codable.playground/Contents.swift
+++ Codable.playground/Contents.swift
@@ -38,7 +38,7 @@ do {
     print(error.localizedDescription)
 }
 
-// MARK: - Decode with decodingSt
+// MARK: - Decode with keyDecodingStrategy
 
 let snake_case_json = """
     {
@@ -52,6 +52,36 @@ jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
 
 do {
     let person = try jsonDecoder.decode(Person2.self, from: snake_case_json)
+    print(person)
+} catch {
+    print(error.localizedDescription)
+}
+
+// MARK: - Custom Decoding (CodingKeys)
+
+let snake_case_json2 = """
+    {
+        "first_name": "Furkan",
+        "last_name": "Vural",
+        "birth_of_date": "26.01.2023"
+    }
+""".data(using: .utf8)!
+
+struct Person3: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "first_name"
+        case surname = "last_name"
+        case birthDate = "birth_of_date"
+    }
+    
+    let name: String
+    let surname: String
+    let birthDate: String
+}
+
+do {
+    let person = try JSONDecoder().decode(Person3.self, from: snake_case_json2)
     print(person)
 } catch {
     print(error.localizedDescription)
